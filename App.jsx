2a2070eed00f99a367b7a521e556af688d91b576
Fumnanya obi <fumnanya.obi@gmail.com>
2024-12-28 14:11:40 -0600

    1st commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main.jsx";
+import Nav from "./components/Nav.jsx";
+
+export default function App() {
+    return (
+        <>
+            <Nav />
+            <Main />
+        </>
+    );
+}
---
+++ components/Nav.jsx
@@ -0,0 +1,13 @@
+export default function Nav() { 
+    return (
+    <header>
+        <nav>
+            <img src="./react-logo.png" alt="logo" />
+            <span>ReactFacts</span>
+        </nav>
+
+    </header>
+             
+) 
+}
+
